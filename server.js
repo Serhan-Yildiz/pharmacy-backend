733815ce02e67f426c9d4629e29a6fc5beb5ac59
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-04-19 22:02:29 +0300

    new version

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.post("/register", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Sunucu çalışıyor: ${PORT}`));
